179aa2b398af9b0145bd7005d0bbd0a795b3800a
Yerusalmi <ryerusalmi@gmail.com>
2017-06-28 01:44:27 +0300

    Some changes
    Some changes

--- js/ap.js
+++ js/ap.js
@@ -1,4 +1,14 @@
 	
+	
+
+
+
+
+
+
+
+	
+	
 	var everything = document.getElementsByTagName('body')[0];
 	var mainDiv = document.createElement('div');
 	everything.appendChild(mainDiv);
@@ -8,6 +18,7 @@
 	mainDiv.appendChild(canvasArea);
 	
 	var buttons = document.createElement('div');
+	
 	mainDiv.appendChild(buttons);
 	
 	
@@ -15,44 +26,68 @@
 	var customizer = document.createElement('div');
 	customizer.id = 'customDetails';
 	
+	// color palatte
+	var colorSpan = document.createElement('span');
+		colorSpan.style.display = 'block';
+		colorSpan.innerHTML = 'Pick a brush color:';
 	
-	var colorInput = document.createElement('input'); // color palatte
-	colorInput.type = 'color';
+	var colorInput = document.createElement('input'); 
+		colorInput.type = 'color';
+	
+	// Background color palatte
+	var colorBgSpan = document.createElement('span');
+		colorBgSpan.style.display = 'block';
+		colorBgSpan.innerHTML = 'Pick a background color:';
+	
+	var colorBgInput = document.createElement('input');
+		colorBgInput.type = 'color';
+	
+	
+	
 	
 	
 	var heighName = document.createElement('span');
 	heighName.style.display = 'block';
-	heighName.innerHTML = 'Custom Brush Height:';
-	var heightcustom = document.createElement('input');
-	heightcustom.id = 'hight';
+	heighName.innerHTML = 'Custom Brush Size:';
+	
+	var radiuscustom = document.createElement('input');
+	radiuscustom.id = 'hight';
+	radiuscustom.setAttribute('type', 'number')
 	var br = document.createElement('br');
 	
-	var widthName = document.createElement('span');
-	widthName.style.display = 'block';
-	widthName.innerHTML = 'Custom Brush Width:';
-	var widthcustom = document.createElement('input');
-	widthcustom.id = 'width';
+
+	
+	var totalSpan = document.createElement('span');
+	totalSpan.style.display = 'block';
+	totalSpan.innerHTML = 'Custom Brush Radius:';
+	var totalRadius = document.createElement('input');
+	totalRadius.id = 'borderradius';
+	totalRadius.setAttribute('type', 'number')
 	
 	
 	buttons.appendChild(customizer);
 	
+	customizer.appendChild(colorSpan);
 	customizer.appendChild(colorInput);
+	customizer.appendChild(colorBgSpan);
+	customizer.appendChild(colorBgInput);
+
 	customizer.appendChild(heighName);
-	customizer.appendChild(heightcustom);
-	customizer.appendChild(br);
-	customizer.appendChild(widthName);
-	customizer.appendChild(widthcustom);
+	customizer.appendChild(radiuscustom);
+
+	customizer.appendChild(totalSpan);
+	customizer.appendChild(totalRadius);
+
 	
 	var originalHeight;
 	var originalWidth;
 
-	var canvasHeight = prompt('Height of your canvas in pixels:', 'Default: 500');
-	var canvasWidth = prompt('Width of your canvas in pixels:', 'Default: 500');
-	var bgColor = prompt('Background Color: (default:white)', 'black,white,grey,lightgrey'); // BG color prompt
+	var canvasHeight = prompt('Height of your canvas in pixels:', 'Default: 350');
+	var canvasWidth = prompt('Width of your canvas in pixels:', 'Default: 350');
 	
-	if ((isNaN(canvasHeight)) || (canvasHeight < 500)) {
-		originalHeight = 500;
-		originalWidth = 500;
+	if ((isNaN(canvasHeight)) || (canvasHeight < 300)) {
+		originalHeight = 350;
+		originalWidth = 350;
 	} else {
 		originalHeight = canvasHeight;
 		originalWidth = canvasWidth;
@@ -66,40 +101,46 @@
 	canvasArea.appendChild(canvas);
 	canvas.style.width = originalHeight + 'px';
 	canvas.style.height = originalWidth + 'px';
-	canvas.style.backgroundColor = bgColor;
+	canvas.style.backgroundColor = 'lightgrey';
 	canvas.style.overflow = 'hidden';
 	canvas.style.position = 'relative';
 	canvas.style.border = '1px solid black';
 	canvas.className = 'hello';
 
-	var customheight = 10;
-	var customwidth = 10;
-
+	var customRadius = 10;
+	var customBorderRadius = 10;
 		
 	// Eraser
 		    
 		 //   var eraser = document.getElementById('myEraser');
 		//    eraser.addEventListener("click", function(){
 			    
-			    
-			    
+		    
+	colorBgInput.addEventListener("change", function() {
+		canvas.style.backgroundColor = colorBgInput.value;	
+	});	
+	
+	
+		    
 			    
 	
 		document.addEventListener("mousemove", function(click){
 			
 			document.addEventListener("change", function() {	//	Custom brush size on empty fields
 				var yourHeight = document.getElementById('hight');
-				if (yourHeight.value === ''){
-					customheight = 10;
+				if (radiuscustom.value === ''){
+					customRadius = 10;
 				} else {
-				customheight = yourHeight.value;
+				customRadius = yourHeight.value;
 				}
-				var yourWidth = document.getElementById('width');
-				if (yourWidth.value === ''){
-					customwidth = 10;
+				
+				var csmRadius = document.getElementById('borderradius');
+				if (totalRadius.value === ''){
+					customBorderRadius = 50;
 				} else {
-				customwidth = yourWidth.value;
+				customBorderRadius = csmRadius.value;
 				}
+				
 			});
 			
 
@@ -115,16 +156,23 @@
 			
 			console.log(click);
 			console.log(coor);
+			var denemeColor = document.getElementById('deneme');
+		    
+		    var screenWidth = document.documentElement.clientWidth;
+		    var newScreen = screenWidth/2
 		    
 		    var kare = document.createElement('div');
 		    canvas.appendChild(kare)
-		    kare.style.height = customheight + 'px';
-		    kare.style.width = customwidth + 'px';
+		    kare.style.height = customRadius + 'px';
+		    kare.style.width = customRadius + 'px';
 		    kare.style.position = 'absolute';
 		    kare.style.backgroundColor = colorInput.value;
 		    kare.style.top = y +'px';
-		    kare.style.left = x-10 +'px';
-		    kare.style.borderRadius = '50px'; // circle
+		    kare.style.left = x +'px';
+		    kare.style.borderRadius = customBorderRadius + 'px'; // circle
+		    
+		    
+		    
 		    
 		    
 		 
